2fbf59d54a3d745a39c74c411fc94978f9aec4eb
saylesss88 <saylesss87@proton.me>
2026-02-02 09:57:30 -0500

    feat: update rust toolchain

--- flake/packages.nix
+++ flake/packages.nix
@@ -1,12 +1,14 @@
-{inputs, ...}: {
-  perSystem = {pkgs, ...}: let
-    craneLib = inputs.crane.mkLib pkgs;
-    # craneLib =
-    #   inputs.crane.lib.${system}.overrideToolchain
-    #   inputs.fenix.packages.${system}.minimal.toolchain;
-  in {
-    packages.default = craneLib.buildPackage {
-      src = craneLib.cleanCargoSource (craneLib.path ../.);
+{ inputs, ... }:
+{
+  perSystem =
+    { pkgs, ... }:
+    let
+      toolchain = pkgs.fenix.complete.toolchain;
+      craneLib = (inputs.crane.mkLib pkgs).overrideToolchain toolchain;
+    in
+    {
+      packages.default = craneLib.buildPackage {
+        src = craneLib.cleanCargoSource (craneLib.path ../.);
+      };
     };
-  };
 }
